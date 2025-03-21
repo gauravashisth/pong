2bed926a8b8dc6bf9393d5150cfec1960d586aea
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-03-21 23:01:35 +0530

    basic game objects: paddles circle divider

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,21 @@
 #include <raylib.h>
 
 int main() {
-  InitWindow(800, 600, "Hello, Raylib!");
+  const int screen_w = 1280;
+  const int screen_h = 800;
 
-  while (!WindowShouldClose()) {
+  InitWindow(screen_w, screen_h, "PONG");
+  SetTargetFPS(60);
+
+  // game loop
+  while (WindowShouldClose() == false) {
+    // created a blank canvas
     BeginDrawing();
-    ClearBackground(RAYWHITE);
-    DrawText("Welcome to Raylib!", 300, 280, 20, DARKGRAY);
+    DrawCircle(screen_w / 2, screen_h / 2, 20, WHITE);
+    // y=screen_h/2-rectangle_h/2
+    DrawRectangle(10, (screen_h / 2) - 60, 25, 120, WHITE);
+    DrawLine(screen_w / 2, 0, screen_w / 2, screen_h, WHITE);
+    DrawRectangle(screen_w - 35, (screen_h / 2) - 60, 25, 120, WHITE);
     EndDrawing();
   }
 
