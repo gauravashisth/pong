1a59512534f8877ced9bf4eb75b24034913083c1
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-03-22 07:25:53 +0530

    class paddle.player

--- src/main.cpp
+++ src/main.cpp
@@ -10,23 +10,47 @@ public:
   void speed() {
     x += speed_x;
     y += speed_y;
+
+    // collision edges for ball
+    if (y + radius >= GetScreenHeight() || y - radius <= 0) {
+      speed_y *= -1;
+    }
+    if (x + radius >= GetScreenWidth() || x - radius <= 0) {
+      speed_x *= -1;
+    }
   }
 };
 
+class Paddle {
+public:
+  int x, y;
+  int width, height;
+  int speed;
+
+  void Draw() { DrawRectangle(x, y, width, height, WHITE); }
+};
+
 Ball ball;
+Paddle player;
 
 int main() {
   const int screen_w = 1280;
   const int screen_h = 800;
 
-  InitWindow(screen_w, screen_h, "PONG");
+  InitWindow(screen_w, screen_h, "Pong");
   SetTargetFPS(60);
 
   ball.radius = 20;
   ball.x = screen_w / 2;
   ball.y = screen_h / 2;
-  ball.speed_x = 7;
-  ball.speed_y = 7;
+  ball.speed_x = 10;
+  ball.speed_y = 10;
+
+  player.width = 25;
+  player.height = 120;
+  player.x = screen_w - player.width - 10;
+  player.y = screen_h / 2 - player.height / 2;
+  player.speed = 6;
 
   // game loop
   while (!WindowShouldClose()) {
@@ -45,8 +69,9 @@ int main() {
     ball.Draw();
 
     // paddles, y=screen_h/2 - rectangle_h/2
-    DrawRectangle(10, (screen_h / 2) - 60, 25, 120, WHITE);
-    DrawRectangle(screen_w - 35, (screen_h / 2) - 60, 25, 120, WHITE);
+    /*DrawRectangle(10, (screen_h / 2) - 60, 25, 120, WHITE);*/
+    /*DrawRectangle(screen_w - 35, (screen_h / 2) - 60, 25, 120, WHITE);*/
+    player.Draw();
 
     EndDrawing();
   }
